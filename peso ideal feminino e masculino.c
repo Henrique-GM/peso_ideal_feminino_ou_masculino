e0368dec966c590a730b8f1716df01b1cbe11d2c
Henrique Gonçalves Mendes <71344359+Henrique-GM@users.noreply.github.com>
2021-02-07 16:43:04 -0300

    Add files via upload

---
+++ peso ideal feminino e masculino.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+		
+	float altura, PesoIdealMasculino, PesoIdealFeminino;	
+	char M, F, S;
+	
+	printf("\n Digite--M--para masculino e--F--para feminino");
+	
+	printf("\n");
+	
+	printf("\n Digite a seu sexo: ");
+	scanf(" %c", &S);
+	
+	printf("\n");
+	
+	printf("\n Digite a sua altura: ");
+	scanf("%f", &altura);
+	
+	printf("\n");
+	
+	if(( S != 'M' ) && ( S != 'F' ))
+	{
+		printf("----digitado errado----Digite em--M--ou--F--");	
+       	  
+	}
+	
+	if(S == 'M')
+	{
+		PesoIdealMasculino = (72.7 * altura) - 58;
+		
+		printf("Seu peso ideal eh:%.2f\n", PesoIdealMasculino);
+		
+
+		if(S == 'F')
+		{
+			PesoIdealFeminino = (62.1 * altura) - 44.7;
+		
+			printf("\n Seu peso ideal eh:%.2f", PesoIdealFeminino);
+		}
+    }
+ 	
+system("pause");	
+return 0;	
+}
